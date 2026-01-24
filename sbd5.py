f570481d69932cf36f3f13acdd25088405065528
Pranay-Kumar-Yadav <pranaykumary9@gmail.com>
2026-01-24 22:10:41 +0530

    swap

---
+++ sbd5.py
@@ -0,0 +1,10 @@
+# to swap two numbers 
+a = input("enter value of a: ")
+b = input("enetr value of b: ")
+
+print('\norginal values \n a=',a, '\n b=' ,b)
+
+a,b = b,a
+
+
+print('\nafter swapping\n a=',a,'\n b=',b)
