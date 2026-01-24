687142bd454322163906acfde3e506d9357701a7
Pranay-Kumar-Yadav <pranaykumary9@gmail.com>
2026-01-24 22:30:33 +0530

    pandn/oande

---
+++ sbd6.py
@@ -0,0 +1,17 @@
+# find given number is positive or negative
+
+num = int(input("insert the number:"))
+
+if num > 0:
+    print("the number is positive")
+else:
+    print("the number is negative")
+    
+
+    #even or odd
+
+    number = int(input("enter a number: "))
+    if (number % 2) == 0:
+        print(number,"is even number")
+    else:
+        print(number,"is odd number")
