dfb28a83adca6e35967ca2764dbe44a5bd2b70c6
Pranay-Kumar-Yadav <pranaykumary9@gmail.com>
2026-01-27 00:05:07 +0530

    prime or not

---
+++ sbd17.py
@@ -0,0 +1,11 @@
+# to calculate factorial of number in python
+num = int(input("enter a number :"))
+count = 0
+fact = 1
+for i in range(1,num+1):
+    if num % i == 0:
+        count = count + 1
+if count == 2:
+    print(num,"is prime number")
+else:
+    print(num,"is not prime number")
