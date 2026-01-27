2ad7d504fc7d2f365de5d6cc7245189be614cfe1
Pranay-Kumar-Yadav <pranaykumary9@gmail.com>
2026-01-27 15:03:16 +0530

    multiple handling exception

---
+++ sbd25.py
@@ -0,0 +1,10 @@
+#handling mulitiple exception
+string = input("enter somthing here: ")
+
+try:
+    num = int(input("enter a number here: "))
+    print (string + num)
+except (ValueError,TypeError) as a:
+    print(a)
+
+print ("thank you")
