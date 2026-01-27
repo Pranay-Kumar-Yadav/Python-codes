6018eab530ca9d08cdb066702ec0e7e2a5f21d92
Pranay-Kumar-Yadav <pranaykumary9@gmail.com>
2026-01-27 13:27:15 +0530

    punc

---
+++ sbd21.py
@@ -0,0 +1,12 @@
+#remove puncutations from a string
+punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+string = input("enter anything here: ")
+
+empty_str = ""
+
+for i in string:
+    if i not in punc:
+        empty_str += i
+
+print (empty_str)
