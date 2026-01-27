0c32279d9b6fb15bf5c3783c7141cb9441d2115f
Pranay-Kumar-Yadav <pranaykumary9@gmail.com>
2026-01-27 13:41:45 +0530

    set operators

---
+++ sbd22.py
@@ -0,0 +1,11 @@
+#illustrate different set operations
+A = {1,2,3,6,8,9}
+B = {3,4,5,6,7,8}
+
+print ("the union is",A | B)
+
+print ("the intersection is ",A & B)
+
+print ("the difference is", A - B)
+
+print ("the symmetric difference is", A ^ B)
