078ba8e2f174750644ba5560dcf1e3849468aa55
Pranay-Kumar-Yadav <pranaykumary9@gmail.com>
2026-01-28 19:16:40 +0530

    interval

---
+++ sbd28.py
@@ -0,0 +1,11 @@
+#prime in an interval
+lower = int(input("enter lower limit here: "))
+upper = int(input("enter upper limit here: "))
+
+for num in range (lower, upper + 1):
+    if num > 1:
+        for i in range (2,num):
+            if num%i == 0:
+                break
+        else:
+            print (num)
