b966b60f7991290ddd6e5eee5a248358f963bf9e
Pranay-Kumar-Yadav <pranaykumary9@gmail.com>
2026-01-28 19:25:44 +0530

    ctof

---
+++ sbd29.py
@@ -0,0 +1,6 @@
+#convert celsius to fahrenheit
+celsius = int(input("enter temperaturenin celsius: "))
+
+fahrenheit = (celsius *(9/5))+32
+
+print ("the converted value is", fahrenheit, "fahrenheit")
