b9d5cb3e3ad7771cc3033243b5616687e3310b47
Pranay-Kumar-Yadav <pranaykumary9@gmail.com>
2026-01-29 13:18:33 +0530

    slicing on list

--- sbd32.py
+++ sbd32.py
@@ -1,4 +1,1 @@
-#slicing on list 
-a = ["vky","pky","bvy","bsy","bly","csy"]
-
-print (a[-6])
+#
---
+++ sbd33.py
@@ -0,0 +1,9 @@
+#slicing on list 
+a = ["vky","pky","bvy","bsy","bly","csy"]
+
+print (a[1::4])
+print (a[::])
+print (a[:3])
+print (a[2:])
+print (a[::2])
+print (a[::-1])
