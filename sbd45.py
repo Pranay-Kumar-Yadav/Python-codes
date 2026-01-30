1cd107beffe74bd00b0fe1b456f2c855c3408b7f
Pranay-Kumar-Yadav <pranaykumary9@gmail.com>
2026-01-30 19:10:16 +0530

    events

---
+++ sbd45.py
@@ -0,0 +1,17 @@
+#events
+
+import threading
+
+event = threading.Event()
+
+def myfunction():
+    print("waiting for event to trigger...\n")
+    event.wait()
+    print("performing action XYZ now...")
+
+t1 = threading.Thread(target=myfunction)
+t1.start()
+
+x = input("do you want to trigger the event? (y/n)\n")
+if x == "y":
+    event.set ()lambda
