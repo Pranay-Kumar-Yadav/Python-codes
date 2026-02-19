4b3bac7836c11779c7b7e36383de071eec8db5b1
Pranay-Kumar-Yadav <pranaykumary9@gmail.com>
2026-02-19 12:47:10 +0530

    anagrams

---
+++ sbd102.py
@@ -0,0 +1,17 @@
+def are_anagrams(str1, str2):
+    # Remove spaces and convert to lowercase
+    str1 = str1.replace(" ", "").lower()
+    str2 = str2.replace(" ", "").lower()
+
+    # Compare sorted characters
+    return sorted(str1) == sorted(str2)
+
+
+
+s1 = input("Enter first string: ")
+s2 = input("Enter second string: ")
+
+if are_anagrams(s1, s2):
+    print("They are anagrams ✅")
+else:
+    print("They are NOT anagrams ")
